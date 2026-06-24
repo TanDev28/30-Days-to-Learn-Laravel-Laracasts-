5d0c99583174624d04b390692d946855d81a8203
TanDev28 <tan01101it@gmail.com>
2026-06-24 12:22:32 +0700

    Create a Layout File Using Laravel Components

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/test', function () {
-    return 'Test page';
+    return view('home');
 });
 
 Route::get('/about', function () {
     return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
