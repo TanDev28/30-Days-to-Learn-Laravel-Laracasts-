0883bf1136ad879e17a4d8cd58a693ae1359e3a4
TanDev28 <tan01101it@gmail.com>
2026-06-27 11:03:31 +0700

    Two Key Eloquent Relationship Types

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -12,4 +12,9 @@ class Job extends Model
     // Quy ước của Eloquent là tên class Job nó sẽ tìm trong database bảng jobs, nếu ko cần dòng bên dưới để lấy chính xác bảng
     protected $table = 'job_listings'; # Để ánh xạ từ bảng job_listings
     protected $fillable = ['title', 'salary']; # Các thuộc tính được cho phép với create()
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
