70bf6fd3c3428c09bafd3226de5b8292cf6472d7
TanDev28 <tan01101it@gmail.com>
2026-06-30 22:31:34 +0700

    Understanding Database Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Job;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -18,8 +19,13 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'Test',
+            'last_name' => 'User',
             'email' => 'test@example.com',
         ]);
+
+        // Job::factory(20)->create(); // Khi không có file JobSeeder.php
+
+        $this->call([JobSeeder::class]);
     }
 }
